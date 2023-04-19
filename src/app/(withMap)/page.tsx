f8abf7fbdc6b39fff385934c1e19d3022df918df
Flávio Sousa <root@fjsousa.com>
2023-04-19 18:47:44 +0100

    root level page is inside router

---
+++ src/app/(withMap)/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import MapboxMap from "../../components/map/Map";
+import Header from "../../components/header/Header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className="bg-pink" style={{ width: "100vw", height: "100vh" }}>
+      <div>
+      <Header />
+      <section>
+        <MapboxMap />
+      </section>
+    </div>
+    </main>
+  );
+}
