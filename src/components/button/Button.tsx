9bfbfe507bb9c9ec12e853d2b738efa436ac22b2
Flávio Sousa <root@fjsousa.com>
2023-04-20 20:17:16 +0100

    button styling

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,15 +1,10 @@
 const Button = (props) => {
   const { name, color } = props;
 
-  
-  return (
-    <button
-      className={`rounded-full p-[0.5rem] border-[2px] solid my-bg-${color}`}
-      style={{ marginInline: '0.5rem', paddingInline: '0.5rem', color: 'white', fontWeight: 'bold'}}
-    >
-      {name}
-    </button>
-  );
+  const classString = `rounded-full p-[0.5rem] my-bg-${color} m-[0.5rem] px-[0.8rem]
+   text-white font-bold`;
+
+  return <button className={classString}>{name}</button>;
 };
 
 export default Button;
